787a92812848f85b6ae1702df961a138e081dc2a
Julian Rojas <julianrojasing@gmail.com>
2018-08-25 19:17:08 -0500

    Generacion pagina en construccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
+import { CapacitacionesPage } from '../pages/capacitaciones/capacitaciones';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    LoginPage
+    LoginPage,
+    CapacitacionesPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    LoginPage
+    LoginPage,
+    CapacitacionesPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/capacitaciones/capacitaciones.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {  NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CapacitacionesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-capacitaciones',
+  templateUrl: 'capacitaciones.html',
+})
+export class CapacitacionesPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
