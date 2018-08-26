02183c0d6c7c8667d68b54f3877c74574ceed185
Julian Rojas <julianrojasing@gmail.com>
2018-08-25 20:50:31 -0500

    Geneacion de clasificados

--- src/pages/clasificados/clasificados.ts
+++ src/pages/clasificados/clasificados.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ClasificadosPage page.
@@ -15,11 +16,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ClasificadosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ClasificadosPage');
+  }
+
+  showAlert(nombre: String, email:String, celular:String) {
+    let dat;
+    dat='<strong>Nombre:</strong>'+nombre+'<br><strong>Telefono:</strong> '+celular+ '<br><strong>Email:</strong> '+email;
+    const alert = this.alertCtrl.create({
+      title: 'Contacto',
+      message: dat,
+      buttons: ['OK'],
+    });
+    alert.present();
   }
 
 }
